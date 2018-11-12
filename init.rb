dad9a126dc4c551c4bd35a32a4a6499d508bcfd1
Mohan <nk.mohan96@gmail.com>
2018-11-12 16:36:52 +0530

    change build 11 to 12

--- init.rb
+++ init.rb
@@ -18,7 +18,7 @@ Redmine::Plugin.register :redmine_loader do
 
   author 'Simon Stearn, Andrew Hodgkinsons'
 
-  desc = 'MS Project/Redmine sync plugin Build11'
+  desc = 'MS Project/Redmine sync plugin Build12'
   name desc
   description desc
 

dfbb1105dee5ff46194ce8b42ab2d09303e8bd5f
Mohan <nk.mohan96@gmail.com>
2019-02-19 19:44:19 +0530

    update build version

--- init.rb
+++ init.rb
@@ -18,7 +18,7 @@ Redmine::Plugin.register :redmine_loader do
 
   author 'Simon Stearn, Andrew Hodgkinsons'
 
-  desc = 'MS Project/Redmine sync plugin Build16'
+  desc = 'MS Project/Redmine sync plugin Build17'
   name desc
   description desc
 

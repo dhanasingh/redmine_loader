9fa0828288b86ab2f605b81dec801e100ab909a0
Arun <arunkumart1995@gmail.com>
2021-01-06 17:41:07 +0530

    Fixed issue list page error

--- lib/loader_other_formats_builder.rb
+++ lib/loader_other_formats_builder.rb
@@ -1,11 +1,12 @@
 module LoaderOtherFormatsBuilder
   def self.included(base)
     base.class_eval do
-      def link_to(name, options={})
-        unless name == 'XML'
-          url = { :format => name.to_s.downcase }.merge(options.delete(:url) || {}).except('page')
-        else
+      def link_to_with_query_parameters(name, url={}, options={})
+        params = @view.request.query_parameters.except(:page, :format).except(*url.keys)
+        if name == 'XML'
           url = { :controller => 'loader', :action => 'export', :query_id => options[:query_id] }
+        else
+          url = {:params => params, :page => nil, :format => name.to_s.downcase}.merge(url)
         end
         caption = options.delete(:caption) || name
         html_options = { :class => name.to_s.downcase, :rel => 'nofollow' }.merge(options)
